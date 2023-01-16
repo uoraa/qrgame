24ef46885f5b25bf7387535bd83975aa634673c4
uoraa <ayendall@gmail.com>
2023-01-16 13:36:32 -0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <qrGenerate msg="QR GENERATOR"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import qrGenerate from './components/qr.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    qrGenerate
   }
 }
 </script>
---
+++ src/components/qr.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="hello">
+        <h1>{{ msg }}</h1>
+        <qrcode-vue :value="value" :size="size" level="L" type="svg" :margin="margin" :background="background" :foreground="foreground" />
+    </div>
+</template>
+
+<script>
+import QrcodeVue from 'qrcode.vue'
+export default {
+    data() {
+        return {
+            value: 'https://example.com',
+            size: 1000,
+            margin: 30,
+            background: "#ff0000",
+            foreground: "#ffffff",
+        }
+    },
+    components: {
+        QrcodeVue,
+    },
+    name: 'qrGenerate',
+    props: {
+        msg: String
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+    margin: 40px 0 0;
+}
+
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+
+li {
+    display: inline-block;
+    margin: 0 10px;
+}
+
+a {
+    color: #42b983;
+}
+</style>
